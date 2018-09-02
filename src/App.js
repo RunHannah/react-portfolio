1dad766bb2712054615673d11e5f866b7a036e26
Hannah Lee <hanlavlee@gmail.com>
2018-09-02 15:12:01 -0700

    Initialize repo.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
+import "../node_modules/materialize-css/dist/js/materialize.min.js";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Hannah's Portfolio</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
