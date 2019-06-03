9c602e0d56f34b0c44d3ad8323fc8c454710a258
Hannah Lee <leexhannah@gmail.com>
2019-06-02 23:43:40 -0700

    Work on landing component and styling.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### Portfolio - in progress.
 
-### Hello! I'm Hannah, a Software Developer specializing in JavaScript, React, Node.js, Express, MongoDB, and PostgreSQL.
+### Hello! I'm Hannah, a Full Stack Developer specializing in JavaScript, React, Node.js, Express, MongoDB, and PostgreSQL.
 
 ### Visit: https://runhannah.github.io/react-portfolio/
--- src/components/landing-page.jsx
+++ src/components/landing-page.jsx
@@ -5,42 +5,37 @@ class LandingPage extends Component {
     return (
       <div className="landing-grid">
         <div className="row">
-          <div className="col s12 m6">
-            <p className="landing-intro">HANNAH LEE</p>
-            <p className="landing-intro-text">
-              Software Developer specializing in JavaScript, React, Node.js,
-              Express, MongoDB, and PostgreSQL, and a background in budget and
-              policy analysis.
-            </p>
-            <div className="center-align social-links-intro">
-              {/* LinkedIn */}
-              <a
-                href="https://linkedin.com/in/hannahlee1"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <i className="fab fa-linkedin" aria-hidden="true" />
-              </a>
+          <p className="landing-intro">HANNAH LEE</p>
+          <p className="landing-intro-text">
+            Full Stack Developer specializing in JavaScript, React, Node.js,
+            Express, MongoDB, PostgreSQL, and problem solving.
+          </p>
+          {/* LinkedIn */}
+          <a
+            href="https://linkedin.com/in/hannahlee1"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i className="fab fa-linkedin" aria-hidden="true" />
+          </a>
 
-              {/* Github */}
-              <a
-                href="https://github.com/runhannah"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <i className="fab fa-github" aria-hidden="true" />
-              </a>
+          {/* Github */}
+          <a
+            href="https://github.com/runhannah"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i className="fab fa-github" aria-hidden="true" />
+          </a>
 
-              {/* Twitter */}
-              <a
-                href="https://twitter.com/tweetbyhannah"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <i className="fab fa-twitter" aria-hidden="true" />
-              </a>
-            </div>
-          </div>
+          {/* Twitter */}
+          <a
+            href="https://twitter.com/tweetbyhannah"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i className="fab fa-twitter" aria-hidden="true" />
+          </a>
         </div>
       </div>
     );
