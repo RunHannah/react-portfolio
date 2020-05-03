64cf0e5be6073d067a0d57e05498ab5bb6afd346
Hannah Lee <leexhannah@gmail.com>
2020-05-03 16:44:43 -0700

    update info

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -17,40 +17,6 @@ class Contact extends Component {
               quickly, I’m highly organized and efficient with a strong
               attention to detail, and excited to join a collaborative team.
             </p>
-
-            <div className='row'>
-              <div className='col s12 m10 l10'>
-                <p>EDUCATION</p>
-
-                <p>
-                  <i className='fas fa-chevron-right' />
-                  Epicodus, Software Development Certificate
-                </p>
-                <p>
-                  <i className='fas fa-chevron-right' />
-                  The George Washington University
-                  <br /> M.A. Public Administration, Budget and Public Finance
-                </p>
-                <p>
-                  <i className='fas fa-chevron-right' />
-                  University of California, Santa Barbara
-                  <br />
-                  B.A. Political Science, International Relations
-                </p>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col s12 m9'>
-                <p>INTERESTS</p>
-                <p>
-                  In my free time I enjoy volunteering, running, playing tennis,
-                  art/music/media, attending tech meetups and building my coding
-                  skills! Living in Los Angeles also provides me time to listen
-                  to my favorite podcasts such as IRL, Without Fail, How I Built
-                  This, Syntax, Asian Not Asian, Reply All, and The Cut.
-                </p>
-              </div>
-            </div>
           </div>
 
           <div id='contact' className='col s12 m12 l5 contact-div'>
@@ -60,10 +26,6 @@ class Contact extends Component {
               <p>
                 <i className='fab fa-linkedin' aria-hidden='true' />
                 https://linkedin.com/in/hannahlee1
-              </p>
-              <p>
-                <i className='far fa-envelope' aria-hidden='true' />
-                leexhannah@gmail.com
               </p>
             </div>
           </div>
